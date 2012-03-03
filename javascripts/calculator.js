38a63e7ec522c01842be7357888412b750980c44
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-03-03 00:21:15 -0500

    starting to work

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -1,16 +1,53 @@
+var answer = 0;
+var evalstr = "";
+var op_in_progress = false;
+
 $(function(){
   $(window).bind("keydown", function(e){
     var key = e.keyCode ? e.keyCode : e.which;
-    
+  
     if(key >= 48 && key <= 57){
       display(display() + String(key - 48));
     } else if(key == 8){
       back();
-    }
+    } 
     
     return false;
   });
   
+  $('.operation').click(function(){
+    var id = $(this).attr('id');
+    
+    evalstr += display();
+    op_in_progress = true;
+    display("");
+    
+    switch(id){
+      case "plus":
+        evalstr += "+";
+        break;
+      case "minus":
+        evalstr += "-";
+        break;
+      case "times":
+        evalstr += "*";
+        break;
+      case "divide":
+        evalstr += "/";
+        break;
+      default:
+        break;
+    }
+    
+  });
+  
+  $('#equals').click(function(){
+    evalstr += display();
+    answer = eval(evalstr);
+    $('p').text(String(answer));
+    evalstr = "";
+    op_in_progress = false;
+  });
   
   $('button.num').click(function(){
     num_pressed($(this).text());
@@ -22,7 +59,7 @@ $(function(){
 });
 
 function display(val){
-  if(val){
+  if(typeof(val) != "undefined"){
     $('p').text(val);
   } else {
     return $('p').text();
